3fcf3254d8ca7e448762f39eef118d8574e36ef7
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-03-19 21:24:33 +0400

    13. Different ways to add default styles

--- app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
+++ app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7129f3999a037b419c4f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c80f5ed0efd819ef034d');
