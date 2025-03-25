980787c2349797f085eee6560a500f426f6a20f3
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-03-25 20:51:59 +0400

    fix bugs in block.json file

--- app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
+++ app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '2d122be33707552cd1cc');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5f2e7a81be3e531e0a70');
