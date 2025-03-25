337b990d23b4261537255793b92db23340fda08e
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-03-25 20:40:30 +0400

    30. Create block variations for the curvy block

--- app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
+++ app/public/wp-content/plugins/curvy/build/blocks/curvy/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0611c3b3b2ba71566935');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a0176a00f7661c06aa1f');
