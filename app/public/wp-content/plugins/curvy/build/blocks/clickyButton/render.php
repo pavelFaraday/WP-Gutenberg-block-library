80edfac2de8e9603419e87b5ea74d314e468c876
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-03-31 12:11:55 +0400

    45. Render dynamic view for clicky button

--- app/public/wp-content/plugins/curvy/build/blocks/clickyButton/render.php
+++ app/public/wp-content/plugins/curvy/build/blocks/clickyButton/render.php
@@ -1,3 +1,10 @@
-<div>
+<?
+    $block_wrapper_attributes = get_block_wrapper_attributes();
+    if ($attributes['linkedPost'] ?? null) {
+        $post_uri = get_permalink($attributes['linkedPost']);
+    }
+?>
+
+<a href="<? echo $post_uri ?? '#' ?>" <? echo $block_wrapper_attributes; ?>>
     <? echo $attributes['labelText']; ?>
-</div>
+</a>
